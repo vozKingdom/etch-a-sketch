a91201dfb435f64a19612aa26dacfd903ae0c952
vozKingdom <161555246+vozKingdom@users.noreply.github.com>
2024-05-20 10:01:11 +0700

    Create 16x16 grid
    - generate 16x16 grid using `for` loop & flexbox.
    - the `for` loop creates 16 flex containers, each
    containing 16 flex items.

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log('Deo Gratias')
+const gridContainer = document.querySelector('.gridContainer') 
+
+
+for (let i = 0; i < 16; i++) {
+
+    const flexContainer = document.createElement('div') 
+
+    gridContainer.appendChild(flexContainer)
+
+    flexContainer.classList.add('flex-container')
+
+
+    for (let i = 0; i < 16; i++) {
+    const flexItem = document.createElement('div');
+
+    flexContainer.appendChild(flexItem)
+
+    flexItem.classList.add('flex-item')
+    
+    flexItem.textContent = `${+i+1}`
+}
+
+}
+
+
+
+
+
+
+
+
