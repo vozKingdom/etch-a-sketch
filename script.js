7dd710fe8afaa68fc1fc5ad98e1217c13d92737a
vozKingdom <161555246+vozKingdom@users.noreply.github.com>
2024-05-21 08:05:26 +0700

    Add button that creates new user inputted grid
    - Add button that allows user to enter a
    custom grid size.
    - The button created in HTML.
    - 'click' event listener registered onto button,
    and when triggered, will execute a function that
    prompts user to pick a grid size. It then deletes
    the current displayed grid, before creating new
    grid using user given input.

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const gridContainer = document.querySelector('.grid-container') 
 
-function createGrid(){
-for (let i = 0; i < 16; i++) {
 
+
+function createGrid(gridSize){   //function that creates grid
+
+for (let i = 0; i < gridSize; i++) {     //create 1 flex container
     const flexContainer = document.createElement('div') 
 
     gridContainer.appendChild(flexContainer)
@@ -10,7 +12,7 @@ for (let i = 0; i < 16; i++) {
     flexContainer.classList.add('flex-container')
 
 
-    for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < gridSize; i++) {   //create 16 flex items, and store in newly minted flex container above
     const flexItem = document.createElement('div');
 
     flexContainer.appendChild(flexItem)
@@ -18,23 +20,58 @@ for (let i = 0; i < 16; i++) {
     flexItem.classList.add('flex-item')
 
     
-    flexItem.addEventListener('mouseover',()=>{
-        flexItem.classList.add('dynamic-background')
-    })
-    flexItem.addEventListener('mouseout',()=>{
-        flexItem.classList.remove('dynamic-background')
-    })
+        flexItem.addEventListener('mouseover',()=>{
+            flexItem.classList.add('dynamic-background')
+        })
+        flexItem.addEventListener('mouseout',()=>{
+            flexItem.classList.remove('dynamic-background')
+        })
+    }
+}
+
 }
 
 
+let defaultGridSize = 16   //set default grid size
 
-}
-}
-
-
-createGrid()
+createGrid(defaultGridSize)   //create grid on page load
 
 
+
+
+
+const btnNewGrid = document.querySelector('button')   //select btn element
+
+btnNewGrid.addEventListener('click', ()=>{   //handle 'click' event of btn  
+
+    for ( ; ; ){   //create infinite loop, until user inputs a number.
+
+        let userGridSize = +prompt('How many squares per side? | --- range: 1-99 --- | 😘', '... for INFINITY (oh-oh-oh)  Cause I love you for INFINITY (oh-oh-oh)')
+
+        if (
+            (!isNaN(userGridSize)) &&   //confirm user given input is a 'number'
+            ( userGridSize < 100 ) &&   //confirm user given input is 'less than' 100
+            ( userGridSize > 0)         //confirm user given input is 'greater than' 0
+        ) {
+            console.log(userGridSize)   //console.log used to debug
+
+            const currentFlexContainers = document.querySelectorAll('.flex-container')   //select current grid
+
+            currentFlexContainers.forEach(()=>{
+                for (i=0; i<currentFlexContainers.length; i++) {
+                    currentFlexContainers[i].remove()   //delete current grid
+                }
+            })
+            
+            createGrid(userGridSize)   //create new grid with user given 'input'
+
+            break;   //break for loop
+        } else {
+            console.log(userGridSize)
+            continue;   //continue for loop
+        }
+    }
+})
 
 
 
